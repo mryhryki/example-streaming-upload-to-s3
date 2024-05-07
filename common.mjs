6f36c15bf029ecbaab75f643695658bbf77c3371
mryhryki <mryhryki@gmail.com>
2024-05-07 17:34:52 +0900

    Update common.mjs

--- common.mjs
+++ common.mjs
@@ -1,3 +1,5 @@
+import AWS from 'aws-sdk';
+
 const getRequiredEnv = (key) => {
   if (!process.env[key]) {
     throw new Error(`Missing required environment variable ${key}`);
@@ -6,5 +8,16 @@ const getRequiredEnv = (key) => {
 };
 
 export const exec = (callback) => {
-  callback({ getRequiredEnv });
+  const bucket = getRequiredEnv("AWS_S3_BUCKET");
+  console.log(`S3 Bucket: ${bucket}`);
+
+  const s3 = new AWS.S3({
+    credentials: {
+      accessKeyId: getRequiredEnv("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: getRequiredEnv("AWS_SECRET_ACCESS_KEY"),
+      getRequiredEnv: process.env["AWS_SESSION_TOKEN"] ?? undefined,
+    },
+  });
+
+  return callback({ bucket, s3 });
 };
