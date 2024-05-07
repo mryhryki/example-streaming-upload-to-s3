74ef05c084900d753ce0d579c611ce55ff7e737a
mryhryki <mryhryki@gmail.com>
2024-05-07 17:34:52 +0900

    Update bucket_delete.mjs

--- bucket_delete.mjs
+++ bucket_delete.mjs
@@ -2,18 +2,7 @@ import AWS from 'aws-sdk';
 import 'dotenv/config';
 import { exec } from './common.mjs';
 
-exec(async ({ getRequiredEnv }) => {
-  const bucket = getRequiredEnv("AWS_S3_BUCKET");
-  console.log(`S3 Bucket: ${bucket}`);
-
-  const s3 = new AWS.S3({
-    credentials: {
-      accessKeyId: getRequiredEnv("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: getRequiredEnv("AWS_SECRET_ACCESS_KEY"),
-      getRequiredEnv: process.env["AWS_SESSION_TOKEN"] ?? undefined,
-    },
-  });
-
+exec(async ({ bucket, s3 }) => {
   const objects = await s3.listObjectsV2({ Bucket: bucket, MaxKeys: 1000 }).promise();
   await Promise.all(objects.Contents.map(async ({ Key }) => {
     await s3.deleteObject({ Bucket: bucket, Key }).promise();
