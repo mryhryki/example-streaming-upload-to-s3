dfbe5ad64cc8c46e718b3e68fa93b710365516f4
mryhryki <mryhryki@gmail.com>
2024-05-07 17:31:21 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,25 +1,19 @@
-import AWS from 'aws-sdk';
-import 'dotenv/config';
+import AWS from "aws-sdk";
+import "dotenv/config";
+import { exec } from "./common.mjs";
 
-const getRequiredEnv = (key) => {
-  if (!process.env[key]) {
-    throw new Error(`Missing required environment variable ${key}`);
-  }
-  return process.env[key];
-};
-
-(async () => {
+exec(async ({ getRequiredEnv }) => {
   const bucket = getRequiredEnv("AWS_S3_BUCKET");
   console.log(`S3 Bucket: ${bucket}`);
 
   const s3 = new AWS.S3({
     credentials: {
-      accessKeyId: getRequiredEnv('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: getRequiredEnv('AWS_SECRET_ACCESS_KEY'),
-      getRequiredEnv: process.env['AWS_SESSION_TOKEN'] ?? undefined,
+      accessKeyId: getRequiredEnv("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: getRequiredEnv("AWS_SECRET_ACCESS_KEY"),
+      getRequiredEnv: process.env["AWS_SESSION_TOKEN"] ?? undefined,
     },
   });
 
   // TODO
   // const response = await s3.putObject({ bucket }).promise()
-})();
+});
