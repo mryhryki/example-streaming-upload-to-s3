0cd5403bd8ce1feaa0f8dc540774853dbd3d986e
mryhryki <mryhryki@gmail.com>
2024-05-07 17:44:47 +0900

    Update bucket_delete.mjs

--- bucket_delete.mjs
+++ bucket_delete.mjs
@@ -1,17 +1,17 @@
-import AWS from 'aws-sdk';
+import {
+  DeleteBucketCommand,
+  DeleteObjectCommand,
+  ListObjectsV2Command,
+} from '@aws-sdk/client-s3';
 import 'dotenv/config';
 import { exec } from './common.mjs';
 
 exec(async ({ bucket, s3 }) => {
-  const objects = await s3.listObjectsV2({ Bucket: bucket, MaxKeys: 1000 }).promise();
-  await Promise.all(objects.Contents.map(async ({ Key }) => {
-    await s3.deleteObject({ Bucket: bucket, Key }).promise();
+  const objects = await s3.send(new ListObjectsV2Command({ Bucket: bucket, MaxKeys: 1000 }));
+  await Promise.all((objects.Contents ?? []).map(async ({ Key }) => {
+    await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key }));
   }));
 
-  const response = await s3.deleteBucket({ Bucket: bucket }).promise();
-  if (response.ok) {
-    console.log("Created bucket: ", response);
-  } else {
-    console.error("Failed to create bucket: ", response);
-  }
+  const response = await s3.send(new DeleteBucketCommand({ Bucket: bucket }));
+  console.log("Deleted bucket: ", response)
 });
