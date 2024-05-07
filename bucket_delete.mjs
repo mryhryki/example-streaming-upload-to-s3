fcac6f3d986dc3128639dcca3aa15d730b09c7c9
mryhryki <mryhryki@gmail.com>
2024-05-07 17:33:20 +0900

    Update bucket_delete.mjs

--- bucket_delete.mjs
+++ bucket_delete.mjs
@@ -1,6 +1,6 @@
-import AWS from "aws-sdk";
-import "dotenv/config";
-import { exec } from "./common.mjs";
+import AWS from 'aws-sdk';
+import 'dotenv/config';
+import { exec } from './common.mjs';
 
 exec(async ({ getRequiredEnv }) => {
   const bucket = getRequiredEnv("AWS_S3_BUCKET");
@@ -14,7 +14,12 @@ exec(async ({ getRequiredEnv }) => {
     },
   });
 
-  const response = await s3.createBucket({ Bucket: bucket }).promise();
+  const objects = await s3.listObjectsV2({ Bucket: bucket, MaxKeys: 1000 }).promise();
+  await Promise.all(objects.Contents.map(async ({ Key }) => {
+    await s3.deleteObject({ Bucket: bucket, Key }).promise();
+  }));
+
+  const response = await s3.deleteBucket({ Bucket: bucket }).promise();
   if (response.ok) {
     console.log("Created bucket: ", response);
   } else {
