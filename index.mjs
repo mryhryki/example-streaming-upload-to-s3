9df050a5e94055b3f3434d9265d5801a79dbf45e
mryhryki <mryhryki@gmail.com>
2024-05-08 02:56:36 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,9 +2,26 @@ import "dotenv/config";
 import { exec } from "./common.mjs";
 import http from "http"
 
+const Port = 50000
+
 exec(async ({ bucket, s3 }) => {
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
+  const server = http.createServer();
+
+  // https://nodejs.org/docs/latest/api/http.html#httpcreateserveroptions-requestlistener
+  server.on('request', (req, res) => {
+    console.log(`${req.method} ${req.url}`)
+    if (req.url === "/") {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        data: 'Hello World!',
+      }));
+    }
+    // TODO: Other routes handings
+
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end("<h1>404 Not Found</h1>");
   });
+
+  console.log(`HTTP server is running at http://localhost:${Port}/`)
+  server.listen(Port)
 });
